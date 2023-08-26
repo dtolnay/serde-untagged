d28edd5d338408fae112e6c7613c2fa2c6865cab
David Tolnay <dtolnay@gmail.com>
2023-08-26 12:26:26 -0700

    Add struct for storing and dropping an arbitrary non-static value

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod error;
 use serde::de::{Deserializer, Expected, Visitor};
 use std::fmt;
 use std::marker::PhantomData;
+use std::mem;
 
 pub use crate::error::Error;
 
@@ -422,5 +423,36 @@ where
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         self.expected.fmt(formatter)
+    }
+}
+
+struct ErasedValue {
+    ptr: *mut (),
+    drop: unsafe fn(*mut ()),
+}
+
+impl ErasedValue {
+    unsafe fn new<T>(value: T) -> Self {
+        ErasedValue {
+            ptr: Box::into_raw(Box::new(value)).cast(),
+            drop: {
+                unsafe fn drop<T>(ptr: *mut ()) {
+                    let _ = Box::from_raw(ptr.cast::<T>());
+                }
+                drop::<T>
+            },
+        }
+    }
+
+    unsafe fn take<T>(self) -> T {
+        let b = Box::from_raw(self.ptr.cast::<T>());
+        mem::forget(self);
+        *b
+    }
+}
+
+impl Drop for ErasedValue {
+    fn drop(&mut self) {
+        unsafe { (self.drop)(self.ptr) }
     }
 }
