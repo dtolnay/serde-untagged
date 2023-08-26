9b996f9c5524b766179949c24d71dd99e542ad55
David Tolnay <dtolnay@gmail.com>
2023-08-25 18:42:46 -0700

    Implement conversion to arbitrary serde error

--- src/error.rs
+++ src/error.rs
@@ -48,3 +48,49 @@ pub enum Unexpected {
     StructVariant,
     Other(String),
 }
+
+impl Error {
+    fn as_serde<E: serde::de::Error>(&self) -> E {
+        match self {
+            Error::Custom(msg) => E::custom(msg),
+            Error::InvalidType {
+                unexpected,
+                expected,
+            } => E::invalid_type(unexpected.as_serde(), &expected.as_str()),
+            Error::InvalidValue {
+                unexpected,
+                expected,
+            } => E::invalid_value(unexpected.as_serde(), &expected.as_str()),
+            Error::InvalidLength { len, expected } => E::invalid_length(*len, &expected.as_str()),
+            Error::UnknownVariant { variant, expected } => E::unknown_variant(variant, expected),
+            Error::UnknownField { field, expected } => E::unknown_field(field, expected),
+            Error::MissingField { field } => E::missing_field(field),
+            Error::DuplicateField { field } => E::duplicate_field(field),
+        }
+    }
+}
+
+impl Unexpected {
+    fn as_serde(&self) -> serde::de::Unexpected {
+        match self {
+            Unexpected::Bool(value) => serde::de::Unexpected::Bool(*value),
+            Unexpected::Unsigned(value) => serde::de::Unexpected::Unsigned(*value),
+            Unexpected::Signed(value) => serde::de::Unexpected::Signed(*value),
+            Unexpected::Float(value) => serde::de::Unexpected::Float(*value),
+            Unexpected::Char(value) => serde::de::Unexpected::Char(*value),
+            Unexpected::Str(value) => serde::de::Unexpected::Str(value),
+            Unexpected::Bytes(value) => serde::de::Unexpected::Bytes(value),
+            Unexpected::Unit => serde::de::Unexpected::Unit,
+            Unexpected::Option => serde::de::Unexpected::Option,
+            Unexpected::NewtypeStruct => serde::de::Unexpected::NewtypeStruct,
+            Unexpected::Seq => serde::de::Unexpected::Seq,
+            Unexpected::Map => serde::de::Unexpected::Map,
+            Unexpected::Enum => serde::de::Unexpected::Enum,
+            Unexpected::UnitVariant => serde::de::Unexpected::UnitVariant,
+            Unexpected::NewtypeVariant => serde::de::Unexpected::NewtypeVariant,
+            Unexpected::TupleVariant => serde::de::Unexpected::TupleVariant,
+            Unexpected::StructVariant => serde::de::Unexpected::StructVariant,
+            Unexpected::Other(msg) => serde::de::Unexpected::Other(msg),
+        }
+    }
+}
