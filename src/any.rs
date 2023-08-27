71f2ea7f823b664071710f5d99ec22cb7aac86b0
David Tolnay <dtolnay@gmail.com>
2023-08-27 10:29:16 -0700

    Compile out type checking in release build

--- src/any.rs
+++ src/any.rs
@@ -1,11 +1,15 @@
+#[cfg(any(debug_assertions, miri))]
 use std::any::{self, TypeId};
+#[cfg(any(debug_assertions, miri))]
 use std::marker::PhantomData;
 use std::mem;
 
 pub(crate) struct ErasedValue {
     ptr: *mut (),
     drop: unsafe fn(*mut ()),
+    #[cfg(any(debug_assertions, miri))]
     type_id: TypeId,
+    #[cfg(any(debug_assertions, miri))]
     type_name: &'static str,
 }
 
@@ -19,19 +23,22 @@ impl ErasedValue {
                 }
                 drop::<T>
             },
+            #[cfg(any(debug_assertions, miri))]
             type_id: non_static_type_id::<T>(),
+            #[cfg(any(debug_assertions, miri))]
             type_name: any::type_name::<T>(),
         }
     }
 
     pub(crate) unsafe fn take<T>(self) -> T {
-        if cfg!(any(debug_assertions, miri)) && self.type_id != non_static_type_id::<T>() {
-            panic!(
-                "ErasedValue mismatch: {} vs {}",
-                self.type_name,
-                any::type_name::<T>(),
-            );
-        }
+        #[cfg(any(debug_assertions, miri))]
+        assert_eq!(
+            self.type_id,
+            non_static_type_id::<T>(),
+            "ErasedValue mismatch: {} vs {}",
+            self.type_name,
+            any::type_name::<T>(),
+        );
 
         let b = Box::from_raw(self.ptr.cast::<T>());
         mem::forget(self);
@@ -45,22 +52,23 @@ impl Drop for ErasedValue {
     }
 }
 
-trait NonStaticAny {
-    fn get_type_id(&self) -> TypeId
-    where
-        Self: 'static;
-}
-
-impl<T: ?Sized> NonStaticAny for PhantomData<T> {
-    fn get_type_id(&self) -> TypeId
-    where
-        Self: 'static,
-    {
-        TypeId::of::<T>()
-    }
-}
-
+#[cfg(any(debug_assertions, miri))]
 fn non_static_type_id<T: ?Sized>() -> TypeId {
+    trait NonStaticAny {
+        fn get_type_id(&self) -> TypeId
+        where
+            Self: 'static;
+    }
+
+    impl<T: ?Sized> NonStaticAny for PhantomData<T> {
+        fn get_type_id(&self) -> TypeId
+        where
+            Self: 'static,
+        {
+            TypeId::of::<T>()
+        }
+    }
+
     let phantom_data = PhantomData::<T>;
     NonStaticAny::get_type_id(unsafe {
         mem::transmute::<&dyn NonStaticAny, &(dyn NonStaticAny + 'static)>(&phantom_data)
