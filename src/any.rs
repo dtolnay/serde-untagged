98f1a276b97b6225f93e9c0d3ec35bd645362c97
David Tolnay <dtolnay@gmail.com>
2024-05-13 23:55:36 -0700

    Use non-static TypeId implementation from typeid crate

--- src/any.rs
+++ src/any.rs
@@ -1,8 +1,6 @@
 use alloc::boxed::Box;
 #[cfg(any(debug_assertions, miri))]
 use core::any::{self, TypeId};
-#[cfg(any(debug_assertions, miri))]
-use core::marker::PhantomData;
 use core::mem;
 
 pub(crate) struct ErasedValue {
@@ -25,7 +23,7 @@ impl ErasedValue {
                 drop::<T>
             },
             #[cfg(any(debug_assertions, miri))]
-            type_id: non_static_type_id::<T>(),
+            type_id: typeid::of::<T>(),
             #[cfg(any(debug_assertions, miri))]
             type_name: any::type_name::<T>(),
         }
@@ -35,7 +33,7 @@ impl ErasedValue {
         #[cfg(any(debug_assertions, miri))]
         assert_eq!(
             self.type_id,
-            non_static_type_id::<T>(),
+            typeid::of::<T>(),
             "ErasedValue mismatch: {} vs {}",
             self.type_name,
             any::type_name::<T>(),
@@ -51,27 +49,4 @@ impl Drop for ErasedValue {
     fn drop(&mut self) {
         unsafe { (self.drop)(self.ptr) }
     }
-}
-
-#[cfg(any(debug_assertions, miri))]
-fn non_static_type_id<T: ?Sized>() -> TypeId {
-    trait NonStaticAny {
-        fn get_type_id(&self) -> TypeId
-        where
-            Self: 'static;
-    }
-
-    impl<T: ?Sized> NonStaticAny for PhantomData<T> {
-        fn get_type_id(&self) -> TypeId
-        where
-            Self: 'static,
-        {
-            TypeId::of::<T>()
-        }
-    }
-
-    let phantom_data = PhantomData::<T>;
-    NonStaticAny::get_type_id(unsafe {
-        mem::transmute::<&dyn NonStaticAny, &(dyn NonStaticAny + 'static)>(&phantom_data)
-    })
 }
