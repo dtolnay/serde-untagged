3097fd8be3d5a6fc295c974cdf27c4d89143a693
David Tolnay <dtolnay@gmail.com>
2025-09-14 09:02:30 -0700

    Release 0.1.9

--- src/lib.rs
+++ src/lib.rs
@@ -183,7 +183,7 @@
 //! ```
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/serde-untagged/0.1.8")]
+#![doc(html_root_url = "https://docs.rs/serde-untagged/0.1.9")]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![allow(
     clippy::doc_markdown,
